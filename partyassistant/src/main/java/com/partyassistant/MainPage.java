8322a897f4f3ee8fdc43980ddeceb2ce572257a9
Dastyronthuyest <matizzprod@gmail.com>
2018-11-05 22:28:56 +0300

    Added main class

--- partyassistant/src/main/java/com/partyassistant/MainPage.java
+++ partyassistant/src/main/java/com/partyassistant/MainPage.java
@@ -1,9 +1,15 @@
 package com.partyassistant;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainPage extends Application {
+
+    private Pane root;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +17,17 @@ public class MainPage extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        root = new Pane();
+        Image fon = new Image(getClass().getClassLoader().getResourceAsStream("fon.jpg"));
+        ImageView fonView = new ImageView(fon);
+        fonView.setFitHeight(680);
+        fonView.setFitWidth(780);
+        root.getChildren().add(fonView);
 
+        Scene scene = new Scene(root,780,680);
+
+        primaryStage.setTitle("Party Assistant");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
