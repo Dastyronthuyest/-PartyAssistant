7e0f2c38be4f218c3773d4223ada3e2df84253fd
Dastyronthuyest <matizzprod@gmail.com>
2018-11-08 12:02:09 +0300

    Added some features

--- partyassistant/src/main/java/com/partyassistant/controller/ViewController.java
+++ partyassistant/src/main/java/com/partyassistant/controller/ViewController.java
@@ -1,9 +1,14 @@
 package com.partyassistant.controller;
 
+import com.partyassistant.dao.GlobalDao;
+import com.partyassistant.entity.GlobalEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewController {
 
@@ -13,8 +18,17 @@ public class ViewController {
     @FXML
     private ComboBox<String> innerCategory;
 
+    private GlobalDao globalDao;
+
     public void initialize() throws Exception {
-        ObservableList<String> options = FXCollections.observableArrayList("Бутерброды", "Алкашка");
+        globalDao = new GlobalDao();
+        List<GlobalEntity> globalEntities = globalDao.findAll();
+        ArrayList<String> globalList = new ArrayList<>();
+        for (GlobalEntity entity: globalEntities) {
+            globalList.add(entity.getName());
+        }
+
+        ObservableList<String> options = FXCollections.observableArrayList(globalList);
         globalCategory.setItems(options);
     }
 }
