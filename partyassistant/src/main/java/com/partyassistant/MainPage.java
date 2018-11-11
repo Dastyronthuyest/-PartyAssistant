3df68adec5e3d501c3d4bd8dcf85566ca4d5f92a
Dastyronthuyest <matizzprod@gmail.com>
2018-11-11 23:46:02 +0300

    Added ingredient list

--- partyassistant/src/main/java/com/partyassistant/MainPage.java
+++ partyassistant/src/main/java/com/partyassistant/MainPage.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
@@ -20,6 +21,7 @@ public class MainPage extends Application {
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view.fxml"));
         MainPage.primaryStage = stage;
+        MainPage.primaryStage.getIcons().add(new Image("/icon.jpg"));
         primaryStage.setTitle("Party Assistant");
         primaryStage.setMaximized(true);
         primaryStage.setScene(new Scene(root));
